a3de28b53ed2b9fb2876f95752db7eaefe8a41ce
Rahul Ashok More <98509088+rahulmore01@users.noreply.github.com>
2023-06-03 09:06:26 +0530

    initial commit

---
+++ basic_functions/code.js
@@ -0,0 +1,11 @@
+// write dow functions for given tasks
+
+// sum of numbers
+function sumOfNumbers(a, b) {
+  return a + b;
+}
+console.log(sumOfNumbers(3, 4));
+// const sumOfNumbers = (a, b) => {
+//   return a + b;
+// };
+// console.log(sumOfNumbers(3, 4));

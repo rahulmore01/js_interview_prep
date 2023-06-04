47818c496d95bee9e7b0d6ab93ca035699971846
Rahul Ashok More <98509088+rahulmore01@users.noreply.github.com>
2023-06-04 08:28:10 +0530

    initial commit

---
+++ basic_functions/array.js
@@ -0,0 +1,10 @@
+// Find the Maximum Number in an Array:
+let arr = [12, 23, 43, 2, 9, 16, 64];
+
+// const maxNumber = (arr) => {
+//   let max = 0;
+//   for (i = 0; (i = arr.length - 1); i++) {
+//     return +i;
+//   }
+// };
+// console.log(maxNumber(arr));
--- basic_functions/code.js
+++ basic_functions/code.js
@@ -1,11 +1,26 @@
 // write dow functions for given tasks
 
 // sum of numbers
-function sumOfNumbers(a, b) {
-  return a + b;
-}
-console.log(sumOfNumbers(3, 4));
+
 // const sumOfNumbers = (a, b) => {
 //   return a + b;
 // };
 // console.log(sumOfNumbers(3, 4));
+
+// reverse a string
+
+// let string = "reverse";
+// function reverseString(string) {
+//   console.log(string.split("").reverse().join(""));
+// }
+// reverseString(string);
+
+// Check if a Number is Even or Odd
+
+// const checkEvenOdd = (num) => {
+//   let check = num % 2 === 1 ? "odd number" : "even number";
+//   return check;
+// };
+// console.log(checkEvenOdd(3));
+
+// Calculate Factorial:
